f003be67a8c4141a3dfbe02b1057f3c989d59419
Micka DJOGHLAL <pro.djoghlal@gmail.com>
2021-07-05 19:01:02 +0200

    2021-07-05 (V2)
    Auth.js middleware Ok

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,7 @@
 const express = require('express'); //On récupère Express
 const router = express.Router(); //On prépare la route user
 
+const auth = require('../middlewares/auth.js'); //On récupère le middleware du token.
 const userCtrl = require('../controllers/user.js'); //On récupère la logique métier concernée.
 
 router.post('/signup', userCtrl.signup); //Création de la route POST signup
