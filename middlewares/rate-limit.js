86356bb7239faa699109b25b15cf5bff4c739eaa
Micka DJOGHLAL <pro.djoghlal@gmail.com>
2021-07-19 19:15:36 +0200

    2021-07-19 (V2)

--- middlewares/rate-limit.js
+++ middlewares/rate-limit.js
@@ -4,5 +4,6 @@ exports.limiter = rateLimit({
     windowMs: 60 * 60 * 1000, //Sanction pendant 1h si tentative de mot de passe
     max: 2 //Nombre de tentative limite.
 
+    //ERREUR 401, Erreur d'authentification.
     //ERREUR 429, trop de requête envoyé par l'utilisateur.
 });
