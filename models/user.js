6b2f7ae243261ae3f4d4ab124590953167595fab
Micka DJOGHLAL <pro.djoghlal@gmail.com>
2021-07-21 17:49:40 +0200

    2021-07-20 (V2)

--- models/user.js
+++ models/user.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const uniqueValidator = require('mongoose-unique-validator');
 
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true }, //On utilise unique pour éviter que deux personnes aient la même adresse email.
     password: { type: String, required: true },
 })
 
+userSchema.plugin(uniqueValidator);
 module.exports = mongoose.model('User', userSchema);
